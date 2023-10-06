615ae50ca9ebdad151c66bb124be05017eaccd2e
Kene Lin <kenelin@gmail.com>
2023-10-06 17:52:51 +0800

    Add basic build script

---
+++ script/build-windows-preset.cmd
@@ -0,0 +1,9 @@
+@echo off
+
+setlocal
+echo [Windows] Applying preset options ...
+set MY_PROJECT_EXPAT_WITH_DISABLED_APPS=ON
+set MY_PROJECT_EXPAT_WITH_DISABLED_TEST_APPS=ON
+echo [Windows] Applying default options ... DONE
+call %~dp0\build-windows.cmd
+endlocal
